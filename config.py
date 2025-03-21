9e960207535427fa5d0da8271201f39486c64485
ToukaSama <blacksummoner6659@gmail.com>
2025-03-21 16:47:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": "",
+  "API_HASH": "ef85493cd32eadcb5309b5957d8d1b86",
   # Get From my.telegram.org
-  "API_ID": "",
+  "API_ID": "22606849",
   #Get For @BotFather
-  "BOT_TOKEN": "",
+  "BOT_TOKEN": "7938878446:AAEpNUP85j8yNe0A6r8crusIpeFUfA3crno",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "",
+  "DATABASE_URL_PRIMARY": "mongodb+srv://meow:meow@meow.a6bo1.mongodb.net/?retryWrites=true&w=majority&appName=meow",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "",
+  "CACHE_CHANNEL": "Hindi_Kochikame",
   # Force Subs Channel username without @
-  "CHANNEL": "",
+  "CHANNEL": "Anime_Sovereign",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
-  # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
+  # Ex : Chapter {chap_num} {chap_name} @Manhwa_Sovereign
   "FNAME": "",
   # Put Thumb Link 
   "THUMB": ""
